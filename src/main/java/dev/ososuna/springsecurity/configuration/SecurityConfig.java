ce727edbc8119bad7c32f0151f8abb9f6f8a8dad
oswi <oswaldo.adrian@live.com.mx>
2022-12-19 21:38:40 -0700

    implement basic authentication

---
+++ src/main/java/dev/ososuna/springsecurity/configuration/SecurityConfig.java
@@ -0,0 +1,24 @@
+package dev.ososuna.springsecurity.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+      .authorizeRequests()
+      .anyRequest()
+      .authenticated()
+      .and()
+      .httpBasic();
+    return http.build();
+  }
+
+}

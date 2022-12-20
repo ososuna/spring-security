fee83329fcbb5b494b1adb93744d0c31d9b28b9e
oswi <oswaldo.adrian@live.com.mx>
2022-12-19 21:15:50 -0700

    greetings controller

---
+++ src/main/java/dev/ososuna/springsecurity/controller/GreetingsController.java
@@ -0,0 +1,22 @@
+package dev.ososuna.springsecurity.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/greetings")
+public class GreetingsController {
+  
+  @GetMapping
+  public ResponseEntity<String> sayHello() {
+    return ResponseEntity.ok("Hello World from our API!");
+  }
+
+  @GetMapping("/goodbye")
+  public ResponseEntity<String> sayGoodbye() {
+    return ResponseEntity.ok("Goodbye World from our API!");
+  }
+
+}

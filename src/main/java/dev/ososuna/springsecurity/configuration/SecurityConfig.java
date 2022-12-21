97baedf3d324d775b9bb0b3ea5799325088e7ef2
oswi <oswaldo.adrian@live.com.mx>
2022-12-20 18:41:23 -0700

    implement authentication controller

--- src/main/java/dev/ososuna/springsecurity/configuration/SecurityConfig.java
+++ src/main/java/dev/ososuna/springsecurity/configuration/SecurityConfig.java
@@ -6,8 +6,10 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -63,6 +65,11 @@ public class SecurityConfig {
     authenticationProvider.setUserDetailsService(userDetailsService());
     authenticationProvider.setPasswordEncoder(passwordEncoder());
     return authenticationProvider;
+  }
+
+  @Bean
+  AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
   }
 
   @Bean
---
+++ src/main/java/dev/ososuna/springsecurity/controller/AuthController.java
@@ -0,0 +1,37 @@
+package dev.ososuna.springsecurity.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.ososuna.springsecurity.model.AuthRequest;
+import dev.ososuna.springsecurity.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+  private final AuthenticationManager authenticationManager;
+  private final UserDetailsService userDetailsService;
+  private final JwtUtil jwtUtil;
+
+  @PostMapping("/authenticate")
+  public ResponseEntity<String> authenticate(@RequestBody AuthRequest request) {
+    authenticationManager.authenticate(
+      new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+    );
+    final UserDetails user = userDetailsService.loadUserByUsername(request.getEmail());
+    if (user != null) {
+      return ResponseEntity.ok(jwtUtil.generateToken(user));
+    }
+    return ResponseEntity.status(400).body("Invalid credentials");
+  }
+}
